14d1c38e5ca08d3c832cb35254263809e544f179
Veljko03 <veljkodovragovic@gmail.com>
2025-02-16 23:59:08 +0100

    created and populated database

--- backend/db/populate.js
+++ backend/db/populate.js
@@ -2,54 +2,83 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 const SQL = `
-CREATE TABLE rooms (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url VARCHAR(255) NOT NULL UNIQUE,
-    room_name TEXT NOT NULL, 
-    expiration_in TIMESTAMP NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
-);
-
 CREATE TABLE users (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    username VARCHAR(255),
+    password_hash VARCHAR(255),
+    google_id VARCHAR(255),
+    profile_image VARCHAR(255),
+    biography TEXT,
+    email VARCHAR(100) UNIQUE
 );
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    content TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE post_likes (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    content TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE
+);
+
+CREATE TABLE comment_likes (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER NOT NULL,
+    comment_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (comment_id) REFERENCES posts (id) ON DELETE CASCADE
+);
+
+CREATE TABLE follows ( 
+  follower INTEGER NOT NULL,
+  following INTEGER NOT NULL,
+  PRIMARY KEY (follower, following),
+  FOREIGN KEY (follower) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (following) REFERENCES posts (id) ON DELETE CASCADE
+);
+
+
 
 CREATE TABLE messages ( 
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    client_offset TEXT UNIQUE,
     content TEXT NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    author_id INTEGER NOT NULL,
-    room_id INTEGER NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (room_id) REFERENCES rooms (id) ON DELETE CASCADE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    sender_id INTEGER NOT NULL,
+    receiver_id INTEGER NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (sender_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (receiver_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-CREATE TABLE user_rooms (
-    user_id INTEGER NOT NULL,
-    room_id INTEGER NOT NULL,
-    PRIMARY KEY (user_id, room_id),
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (room_id) REFERENCES rooms (id) ON DELETE CASCADE
-);
+
 
 
 `;
 
 async function main() {
   console.log("seeding...");
-  const client = null;
-  if (process.env.NODE_ENV === "production") {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-    });
-  } else if (process.env.NODE_ENV === "development") {
-    client = new Client({
-      connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
-    });
-  }
+
+  const client = new Client({
+    //connectionString: process.env.DATABASE_URL,
+    connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
+  });
 
   await client.connect();
   await client.query(SQL);
