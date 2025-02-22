2bd9edf2a04565c59b28e1f1dd01202f284293a4
Veljko03 <veljkodovragovic@gmail.com>
2025-02-22 15:58:34 +0100

    setted condtional rednring based if user exist in localstorage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./index.css";
 import SideBar from "./Sidebar";
+import { useEffect, useState } from "react";
 function App() {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const sotredUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
+    if (sotredUser) {
+      setUser(JSON.parse(sotredUser));
+    } else {
+      navigate("/auth/log-in");
+    }
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  if (!user) return null;
   return (
     <div className="appContainer">
       <div className="header">
--- frontend/src/public/Log-in.jsx
+++ frontend/src/public/Log-in.jsx
@@ -1,18 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import "./public.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const LogPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const API_URL = import.meta.env.VITE_BACKEND_APP_API_URL;
+
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   fetch(`${API_URL}/auth/oauth2/redirect/google`, {
+  //     method: "get",
+  //     mode: "cors",
+  //   })
+  //     .then((respose) => respose.json())
+  //     .then((data) => console.log(data));
+  // }, []);
 
   const handleLogIn = (e) => {
     e.preventDefault();
     if (email == "" || password == "") {
       alert("Enter values first");
     }
+
+    const user = { email, password };
+    fetch(`${API_URL}/auth/log-in`, {
+      method: "post",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          setEmail("");
+          setPassword("");
+          alert("Wrong email or password");
+          return;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data) {
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("user", JSON.stringify(data.user));
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
   };
 
   return (
@@ -20,8 +61,7 @@ const LogPage = () => {
       <div className="topTitle">Insta Clone</div>
       <div className="formContainer">
         <div className="section1">
-          <div>Create New</div>
-          <div>Acoount</div>
+          <div>Login</div>
         </div>
         <form onSubmit={handleLogIn} className="section2">
           <Input
@@ -38,8 +78,9 @@ const LogPage = () => {
           <Button label={"Log up"} />
         </form>
         <div className="section3">
+          <a href="http://localhost:3000/auth/google">Google</a>
           <h1>Don't have an account?</h1>
-          <Link to={"/auth/sign-in"}>Login</Link>
+          <Link to={"/auth/sign-in"}>Sign up</Link>
         </div>
       </div>
     </div>
