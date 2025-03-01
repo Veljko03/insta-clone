13666cf0b2119346ec59c5d45874833cee81db1a
Veljko03 <veljkodovragovic@gmail.com>
2025-03-01 16:51:34 +0100

    displayed porfiles num of followers, name and bio

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import "./pages.css";
 import { useOutletContext, useParams, useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
-  //fetchovati profil
-  //br postova, followera
+  const [token, user] = useOutletContext();
+  const [profile, setProfile] = useState(null);
+
+  const API_URL = import.meta.env.VITE_BACKEND_APP_API_URL;
+  useEffect(() => {
+    if (!token) return;
+    const id = user.id;
+    fetch(`${API_URL}/user/${id}`, {
+      method: "get",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        setProfile(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  if (!profile) return;
+  console.log(profile);
+
   return (
-    <div className="homeContainer">
+    <div className="profileCon">
       <div className="goBack">
         <button className="backBtn" onClick={() => navigate("/")}>
           Go back
@@ -15,7 +39,26 @@ const ProfilePage = () => {
       <div className="profile">
         <div className="picAndFollow">
           <h2>img</h2>
-          <div className="followers">foll</div>
+          <div className="followers">
+            <div className="profileName">{profile.username}</div>
+            <div className="f">
+              <div className="fCon1">
+                <p>{profile.followers}</p>
+                <p>Followers</p>
+              </div>
+              <div className="fCon1">
+                <p>{profile.following}</p>
+                <p>Following</p>
+              </div>
+              <div className="fCon1">
+                <p>0</p>
+                <p>Posts</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bio">
+          <p>{profile.biography}aaaaaaaaa</p>
         </div>
       </div>
       <div className="posts"></div>
