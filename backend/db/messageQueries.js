7beeb4ed1a6aa9f1b19a20beafb96dcb95105cf0
Veljko03 <veljkodovragovic@gmail.com>
2025-03-08 14:35:45 +0100

    implemented recovery messages and emmititng mess by id

--- backend/db/messageQueries.js
+++ backend/db/messageQueries.js
@@ -9,4 +9,63 @@ const sendMessage = async (content, sender, reciever) => {
   return result.rows[0];
 };
 
-module.exports = { sendMessage };
+const getMessagesById = async (id) => {
+  const result = await pool.query(
+    `SELECT 
+    m.id AS message_id,
+    m.content,
+    m.created_at,
+    m.sender_id,
+    m.receiver_id,
+    sender.username AS sender_name,
+    sender.profile_image AS sender_image,
+    receiver.username AS receiver_name,
+    receiver.profile_image AS receiver_image
+FROM 
+    messages m
+INNER JOIN 
+    users sender ON m.sender_id = sender.id
+INNER JOIN 
+    users receiver ON m.receiver_id = receiver.id
+WHERE 
+    m.id=$1
+ORDER BY 
+    m.created_at ASC;
+`,
+    [id]
+  );
+
+  return result.rows[0];
+};
+const getMessages = async (sender, reciever) => {
+  const result = await pool.query(
+    `SELECT 
+    m.id AS message_id,
+    m.content,
+    m.created_at,
+    m.sender_id,
+    m.receiver_id,
+    sender.username AS sender_name,
+    sender.profile_image AS sender_image,
+    receiver.username AS receiver_name,
+    receiver.profile_image AS receiver_image
+FROM 
+    messages m
+INNER JOIN 
+    users sender ON m.sender_id = sender.id
+INNER JOIN 
+    users receiver ON m.receiver_id = receiver.id
+WHERE 
+    (m.sender_id = $1 AND m.receiver_id = $2) 
+    OR 
+    (m.sender_id = $2 AND m.receiver_id = $1)
+ORDER BY 
+    m.created_at ASC;
+`,
+    [sender, reciever]
+  );
+
+  return result.rows;
+};
+
+module.exports = { sendMessage, getMessages, getMessagesById };
--- backend/socket.js
+++ backend/socket.js
@@ -1,6 +1,10 @@
 const { Server } = require("socket.io");
 const dotenv = require("dotenv");
-const { sendMessage } = require("./db/messageQueries");
+const {
+  sendMessage,
+  getMessages,
+  getMessagesById,
+} = require("./db/messageQueries");
 const envFile =
   process.env.NODE_ENV === "production"
     ? ".env.production"
@@ -14,17 +18,31 @@ const initializeSocket = (server) => {
       origin: process.env.URL,
     },
   });
-  io.on("connection", (socket) => {
-    socket.on("chat-message", (msg) => {
+  io.on("connection", async (socket) => {
+    socket.on("chat-message", async (msg) => {
+      let result;
       try {
         //poslati msg u bazu tj kreirati poruku
         const content = msg.msg;
         const sender = msg.sender;
         const reciever = msg.reciver;
-        console.log(msg);
 
-        sendMessage(content, sender, reciever);
-      } catch (error) {}
+        const m = await sendMessage(content, sender, reciever);
+        result = await getMessagesById(m.id);
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
+      io.emit("chat-message", result);
+    });
+    socket.on("load-messages", async ({ senderId, receiverId }) => {
+      try {
+        // Preuzmi poruke iz baze između dva korisnika
+        const messages = await getMessages(senderId, receiverId);
+        // Pošalji poruke nazad klijentu
+        socket.emit("load-messages", messages);
+      } catch (error) {
+        console.error("Error loading messages:", error);
+      }
     });
   });
 
